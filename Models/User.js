632e2ef8ba7e231e84db6d530418aea1a6164c9c
Hrutvik66 <hrutvikmalshikare66@gmail.com>
2023-02-22 20:54:23 +0530

    Minor deployment change

--- Models/User.js
+++ Models/User.js
@@ -23,38 +23,12 @@ const createBreeder = ({ farm_type, user_id, license }, callback) => {
   );
 };
 
-// // Create Role
-// const createRole = ({ role_name }) => {
-//   connection.query(
-//     `INSERT INTO bre_user_role IF NOT EXIST (role_name) VALUES ("${role_name}")`,
-//     function (err, res) {
-//       console.log(err);
-//     }
-//   );
-// };
-
-// // Create master farm 
-// const createMasterFarm = ({ farm_name, farm_id, farm_description }) => {
-//   connection.query(
-//     `INSERT INTO bre_farm IF NOT EXIST (farm_id,farm_name,farm_description) VALUES ("${farm_id}","${farm_name}","${farm_description}")`,
-//     function (err, res) {
-//       console.log(err);
-//     }
-//   );
-// };
 
 // Register user
 const createUser = (
   { userName, userType, password, email, contact, aadhar },
   callback
 ) => {
-  // createRole({ role_name: "admin" });
-  // createRole({ role_name: "breeder" });
-  // createRole({ role_name: "individual" });
-
-  // createMasterFarm({ farm_name: "stub", farm_id: 1, farm_description: "stub" });
-  // createMasterFarm({ farm_name: "cattery", farm_id: 2, farm_description: "cattery" });
-  // createMasterFarm({ farm_name: "kennel", farm_id: 3, farm_description: "kennel" });
 
   var type;
   if (userType == "breeder") type = 2;
@@ -64,7 +38,6 @@ const createUser = (
     `INSERT INTO bre_user (userName,user_type_id,password,email,contact,aadhar,user_status) VALUES ("${userName}","${type}","${password}","${email}","${contact}","${aadhar}","pending_verification")`,
     function (err, res) {
       if (err) {
-        console.log("register" + err);
         return callback(null);
       }
       return callback(JSON.parse(JSON.stringify(res)).insertId);
@@ -77,16 +50,16 @@ const loginUser = ({ email, password }, callback) => {
   connection.query(
     `SELECT * FROM bre_user WHERE email = "${email}"`,
     function (err, res) {
-      if (res.length == 0 || err) {
+      if (res?.length == 0 || err) {
         return callback(null);
-      } else if (res[0].user_status == "pending_verification") {
+      } else if (res[0]?.user_status == "pending_verification") {
         return callback("pending_verification");
       } else {
         // Validate password
         const password_auth = bcrypt.compareSync(password, res[0].password);
         if (!password_auth) return callback(null);
         // Return user
-        if (res[0].userType == "breeder") {
+        if (res[0]?.userType == "breeder") {
           getBreeder(email, (user) => {
             return callback(user);
           });
